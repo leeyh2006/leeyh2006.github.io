53d3d3f8a9481d7c901f7e6a61124b7d46ad70c6
82108 <leeyh2006@nate.com>
2019-05-03 16:50:21 +0900

    potfolio

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,36 @@
-// Load plugins
-const gulp = require("gulp");
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var wait = require('gulp-wait');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
 
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function(cb) {
-
-  // Start Bootstrap Clean Blog SCSS
-  gulp.src([
-      './node_modules/startbootstrap-clean-blog/scss/**/*'
-    ])
-    .pipe(gulp.dest('./assets/vendor/startbootstrap-clean-blog/scss'))
-
-  // Start Bootstrap Clean Blog JS
-  gulp.src([
-      './node_modules/startbootstrap-clean-blog/js/clean-blog.min.js',
-      './node_modules/startbootstrap-clean-blog/js/jqBootstrapValidation.js'
-    ])
-    .pipe(gulp.dest('./assets/vendor/startbootstrap-clean-blog/js'))
-
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./assets/vendor/bootstrap'))
-
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./assets/vendor/jquery'))
-
-  // Font Awesome
-  gulp.src([
-      './node_modules/@fortawesome/**/*',
-    ])
-    .pipe(gulp.dest('./assets/vendor'))
-
-  cb();
-
+gulp.task('scripts', function() {
+    return gulp.src('js/scripts.js')
+        .pipe(plumber(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        })))
+        .pipe(uglify({
+            output: {
+                comments: '/^!/'
+            }
+        }))
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('js'));
 });
 
-gulp.task("default", gulp.parallel('vendor'));
+gulp.task('styles', function () {
+    return gulp.src('./scss/styles.scss')
+        .pipe(wait(250))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('js/scripts.js', gulp.series('scripts'));
+    gulp.watch('scss/styles.scss', gulp.series('styles'));
+});
